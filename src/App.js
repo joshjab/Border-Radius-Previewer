4beaa1d10c072207a3da50f8a946f126b18a591d
j.jablonowski <jjablonowski@trideum.com>
2019-08-19 14:31:00 +0000

    Added text box inputs, need to seperate onChange script

--- src/App.js
+++ src/App.js
@@ -14,15 +14,23 @@ function App() {
         <Card style={{ width: '25rem' }}>
 	  <Container>
 	    <Row>
-	      <Col className="text-left">1</Col>
-              <Col className="text-right">2</Col>
+	      <Col className="text-left">
+		<input style={{ width: '3rem' }} type="text" name="s1" value="4" />
+	      </Col>
+              <Col className="text-right">
+		<input style={{ width: '3rem' }} type="text" name="s2" value="4" />
+              </Col>
 	    </Row>
     	  </Container>
 	  <h1 id="rounded-box">Your content here</h1>
 	  <Container>
             <Row>
-              <Col className="text-left">3</Col>
-              <Col className="text-right">4</Col>
+              <Col className="text-left">
+		<input style={{ width: '3rem' }} type="text" name="s3" value="4" />
+	      </Col>
+              <Col className="text-right">
+		<input style={{ width: '3rem' }} type="text" name="s4" value="4" />
+	      </Col>
             </Row>
           </Container>
 	  <Card.Text style={{ textAlign: 'center', padding: '10px' }}>Change the sliders below to change the borders!</Card.Text>
